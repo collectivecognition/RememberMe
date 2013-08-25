2b1530a39888c5467994fe33c90890cf433c5193
William Blanchette <wb@collectivecognition.com>
2013-08-25 00:22:42 -0400

    Bedtime...

--- remember_me.py
+++ remember_me.py
@@ -1,8 +1,8 @@
-import pygame, random, sys
+import math, pygame, random, sys
 from pygame.locals import *
 
 width, height = 640, 480
-max_shape_radius = 40.0
+min_shape_radius, max_shape_radius = 40.0, 100.0
 
 pygame.init()
 fpsClock = pygame.time.Clock()
@@ -22,17 +22,20 @@ colors = {
 shape_colors = ["white", "red", "green", "blue"]
 
 def circle(shape):
-	return pygame.draw.circle(window, shape["color"], shape["position"], int(max_shape_radius))
+
+	return pygame.draw.circle(window, shape["color"], shape["position"], shape["radius"])
 
 def square(shape):
-	return pygame.draw.rect(window, shape["color"], (shape["position"]["x"] - shape["radius"], shape["position"]["y"] - shape["radius"] / 2, shape["radius"] * 2, shape["radius"] * 2))
+	"""Create a square that will fit inside a circle with radius shape.radius"""
+	side_length = math.sqrt(shape["radius"] ** 2 / 2)
+	return pygame.draw.rect(window, shape["color"], (shape["position"][0] - side_length / 2, shape["position"][1] - side_length / 2, side_length, side_length))
 
 # Global variables to store state information
 
 shapes = [circle, square]
 
-mousex, mousey = 0, 0
-num_shapes = 3
+mouse_x, mouse_y = 0, 0
+num_shapes = 10
 
 level_shapes = []
 
@@ -48,38 +51,57 @@ def get_level_shape(position):
 	"""Get shape based on x, y coordinates"""
 	return level_shapes[get_level_shapes_index(position)]
 
-def distanceBetween(a, b):
+def distance_between(a, b):
 	"""Calculate the distance between two points"""
-	return math.sqrt((a["x"] - b["x"]) ** 2 + (a["y"] - b["y}"]) ** 2))
+	return math.hypot(a[0] - b[0], a[1] - b[1])
 
-def doesShapeCollideWithOthers(shape):
+def is_shape_in_bounds(shape):
+	if shape["position"][0] > shape["radius"] and shape["position"][0] < width - shape["radius"]:
+		if shape["position"][1] > shape["radius"] and shape["position"][1] < height - shape["radius"]:
+			return True
+	return False
+
+def does_shape_collide_with_others(shape):
 	"""Check whether a shape overlaps the area (radius) of any others"""
 	global level_shapes
 	for other in level_shapes:
-		if distanceBetween(shape["position"], other["position"]) < math.abs(shape["radius"] + other["radius"]):
-			break:
+		if distance_between(shape["position"], other["position"]) < shape["radius"] + other["radius"]:
+			break
 	else:
-		return false
-	return true
+		return False
+	return True
 
 def generate_level():
 	"""Generate a new level"""
 	global level_shapes
 
-	for s in num_shapes:
-		shape = {
-			"x": random.range(0, width),
-			"y": random.range(0, height),
-			"radius": random.range(0, max_shape_radius),
-			"color": colors[random.choice(shape_colors)],
-			"shape": random.choice(shapes),
-		}
+	for s in xrange(num_shapes):
+		shape = None
+		while True: # FIXME: Not terribly pythonic :P
+			print "Trying..."
+			shape = {
+				"position": [
+					random.randint(0, width),
+					random.randint(0, height),
+				],
+				"radius": random.randint(min_shape_radius, max_shape_radius),
+				"color": colors[random.choice(shape_colors)],
+				"shape": random.choice(shapes),
+			}
+			if not does_shape_collide_with_others(shape):
+				if is_shape_in_bounds(shape):
+					break
+		level_shapes.append(shape)
 
 def draw_level():
 	"""Draw the shapes in the current level"""
 	for s in level_shapes:
 		if s["shape"] != None:
 			s["shape"](s)
+
+def next_level():
+	"""Increment difficulty before generating the next level"""
+	pass
 
 # Generate initial level
 
@@ -95,9 +117,17 @@ while True:
 			pygame.quit()
 			sys.exit()
 		elif event.type == MOUSEMOTION:
-			mousex, mousey = event.pos
+			mouse_x, mouse_y = event.pos
 
 	draw_level()
+
+	circle({
+		"position": [mouse_x, mouse_y],
+		"radius": 10,
+		"color": colors["white"]
+	})
+
+	window.set_alpha(100)
 
 	pygame.display.update()
 	fpsClock.tick(30)
